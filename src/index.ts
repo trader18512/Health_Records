ae82f5b2eb41e393fa3471cce16e85ac0e0369ce
vishnuvishwakarma <vv083150@gmail.com>
2023-11-21 00:04:19 +0530

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,195 @@
-import { query, update, Canister, text, Record, StableBTreeMap, Ok, None, Some, Err, Vec, Result, nat64, ic, Opt, Variant } from 'azle';
+import {
+    $query,
+    $update,
+    Record,
+    StableBTreeMap,
+    Result,
+    nat64,
+    ic,
+    match,
+    Variant,
+    Vec,
+    Opt,
+    text,
+  } from "azle";
+  import { v4 as uuidv4 } from "uuid";
+  
+  // Define the Patient type
+  type Patient = Record<{
+    id: string;
+    name: string;
+    age: nat64;
+    gender: string;
+    bloodType: string;
+    medicalHistory: string;
+    createdAt: nat64;
+    updatedAt: Opt<nat64>;
+  }>;
+  
+  // Define the PatientPayload type for creating or updating patients
+  type PatientPayload = Record<{
+    name: string;
+    age: nat64;
+    gender: string;
+    bloodType: string;
+    medicalHistory: string;
+  }>;
+  
+  // Define the Error type
+  type Error = Variant<{
+    NotFound: string;
+    InvalidPayload: string;
+  }>;
+  
+  // Create StableBTreeMap to store patients
+  const patientsStorage = new StableBTreeMap<string, Patient>(0, 44, 1024);
+  
+  // Function to add patients in the storage
+  $update;
+  export function addPatient(payload: PatientPayload): Result<Patient, Error> {
+    try {
+      // Payload Validation
+      if (!payload.name || !payload.age || !payload.gender || !payload.bloodType || !payload.medicalHistory) {
+        return Result.Err({ InvalidPayload: "Invalid patient payload" });
+      }
 
-import { v4 as uuidv4 } from 'uuid';
+      // Validate age
+      if (payload.age <= 0) {
+        return Result.Err({ InvalidPayload: "Invalid age" });
+      }
 
-const PatientPayload = Record({ name: text, age: nat64, gender: text, bloodType: text, medicalHistory: text });
+      // Validate gender
+      if (payload.gender !== "male" && payload.gender !== "female") {
+        return Result.Err({ InvalidPayload: "Invalid gender" });
+      }
 
-const Patient = Record({ id: text, name: text, age: nat64, gender: text, bloodType: text, medicalHistory: text, createdAt: nat64, updatedAt: Opt(nat64) });
+      // Validate blood type
+      if (payload.bloodType !== "A" && payload.bloodType !== "B" && payload.bloodType !== "AB" && payload.bloodType !== "O") {
+        return Result.Err({ InvalidPayload: "Invalid blood type" });
+      }
 
-const Error = Variant({ NotFound: text, InvalidPayload: text, });
+      // Validate medical history
+      if (payload.medicalHistory.length > 1000) {
+        return Result.Err({ InvalidPayload: "Invalid medical history" });
+      }
 
-const patientsStorage = StableBTreeMap(text, Patient, 0);
+      // Generate UUID, create and update time, and add the payload
+      const patient: Patient = {
+        id: uuidv4(),
+        createdAt: ic.time(),
+        updatedAt: Opt.None,
+        name: payload.name,
+        age: payload.age,
+        gender: payload.gender,
+        bloodType: payload.bloodType,
+        medicalHistory: payload.medicalHistory
+    
+      };
 
-export default Canister({
+      // Insert the patient
+      patientsStorage.insert(patient.id, patient);
 
-//Below we add the Patient to patientsStorage
-addPatient: update([PatientPayload], Result(Patient, Error), (payload) => {
-    //generate uuid, create and update time and our payload
-    const patient = { id: uuidv4(), createdAt: ic.time(), updatedAt: None, ...payload };
-    //Insert the patient
-    patientsStorage.insert(patient.id, patient);
-    //return an OK with the patient you saved
-    return Ok(patient);
-}),
-
-//Below we get all patients from the storage
-getPatients: query([], Result(Vec(Patient), Error), () => {
-    return Ok(patientsStorage.values());
-}),
-
-//we get specific patient from the patients storage, we provide the uuid
-getPatient: query([text], Result(Patient, Error), (id) => {
-    const patientOpt = patientsStorage.get(id);
-    if ("None" in patientOpt) {
-        return Err({ NotFound: `the patient with id=${id} not found` });
+      // Return an OK with the patient you saved
+      return Result.Ok(patient);
+    } catch (error) {
+      return Result.Err({ InvalidPayload: `Failed to add patient: ${error}` });
     }
-    return Ok(patientOpt.Some);
-}),
-
-//Update a patient already in the patientsStorage, we provide a uuid
-updatePatient: update([text, PatientPayload], Result(Patient, Error), (id, payload) => {
-    const patientOpt = patientsStorage.get(id);
-    if ("None" in patientOpt) {
-        return Err({ NotFound: `couldn't update a patient with id=${id}. patient not found` });
+  }
+  
+  $query;
+  // Function to get all patients from the storage
+  export function getPatients(): Result<Vec<Patient>, Error> {
+    try {
+      // Return all patients
+      return Result.Ok(patientsStorage.values());
+    } catch (error) {
+      return Result.Err({ NotFound: `Failed to get patients: ${error}` });
     }
-    const patient = patientOpt.Some;
-    const updatedPatient = { ...patient, ...payload, updatedAt: Some(ic.time()) };
-    patientsStorage.insert(patient.id, updatedPatient);
-    return Ok(updatedPatient);
-}),
-
-//delete a patient from the patientsStorage, we provide a uuid to remove
-deletePatient: update([text], Result(Patient, Error), (id) => {
-    const deletedPatient = patientsStorage.remove(id);
-    if ("None" in deletedPatient) {
-        return Err({ NotFound: `couldn't delete a patient with id=${id}. patient not found` });
+  }
+  
+  $query;
+  // Function to get a specific patient from the patients storage using the UUID
+  export function getPatient(id: string): Result<Patient, Error> {
+    try {
+      // ID Validation
+      if (!id || typeof id !== "string") {
+        return Result.Err({ NotFound: "Invalid patient ID" });
+      }
+  
+      const existingPatientOpt = patientsStorage.get(id);
+  
+      return match(existingPatientOpt, {
+        Some: (patient) => Result.Ok<Patient, Error>(patient),
+        None: () => Result.Err<Patient, Error>({ NotFound: `The patient with ID ${id} not found` }),
+      });
+    } catch (error) {
+      return Result.Err({ NotFound: `Failed to get patient: ${error}` });
     }
-    return Ok(deletedPatient.Some);
-})
-
-});
-
-globalThis.crypto = { 
-    // @ts-ignore 
-    getRandomValues: () => { let array = new Uint8Array(32);
-
-    for (let i = 0; i < array.length; i++) {
+  }
+  
+  $update;
+  // Function to update a patient already in the patientsStorage using the UUID
+  export function updatePatient(id: string, payload: PatientPayload): Result<Patient, Error> {
+    try {
+      // ID Validation
+      if (!id || typeof id !== "string") {
+        return Result.Err({ NotFound: "Invalid patient ID" });
+      }
+  
+      // Payload Validation
+      if (!payload.name || !payload.age || !payload.gender || !payload.bloodType || !payload.medicalHistory) {
+        return Result.Err({ InvalidPayload: "Invalid patient payload" });
+      }
+  
+      const existingPatientOpt = patientsStorage.get(id);
+  
+      return match(existingPatientOpt, {
+        Some: (existingPatient) => {
+          const updatedPatient: Patient = {
+            ...existingPatient,
+            ...payload,
+            updatedAt: Opt.Some(ic.time()),
+          };
+  
+          patientsStorage.insert(id, updatedPatient);
+          return Result.Ok<Patient, Error>(updatedPatient);
+        },
+        None: () => Result.Err<Patient, Error>({ NotFound: `Couldn't update a patient with ID ${id}. Patient not found` }),
+      });
+    } catch (error) {
+      return Result.Err({ InvalidPayload: `Failed to update patient: ${error}` });
+    }
+  }
+  
+  $update;
+  // Function to delete a patient from the patientsStorage using the UUID
+  export function deletePatient(id: string): Result<Patient, Error> {
+    try {
+      // ID Validation
+      if (!id || typeof id !== "string") {
+        return Result.Err({ NotFound: "Invalid patient ID" });
+      }
+  
+      const deletedPatientOpt = patientsStorage.remove(id);
+  
+      return match(deletedPatientOpt, {
+        Some: (deletedPatient) => Result.Ok<Patient, Error>(deletedPatient),
+        None: () => Result.Err<Patient, Error>({ NotFound: `Couldn't delete a patient with ID ${id}. Patient not found` }),
+      });
+    } catch (error) {
+      return Result.Err({ NotFound: `Failed to delete patient: ${error}` });
+    }
+  }
+  
+  // A workaround to make the uuid package work with Azle
+  globalThis.crypto = {
+    // @ts-ignore
+    getRandomValues: () => {
+      let array = new Uint8Array(32);
+      for (let i = 0; i < array.length; i++) {
         array[i] = Math.floor(Math.random() * 256);
-    }
-
-    return array;
-}
-
-};
+      }
+      return array;
+    },
+  };
+  
